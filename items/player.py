d9eaa2e710c63a441e47ebd53fa26b88239f0684
JoshLeung6 <56309368+JoshLeung6@users.noreply.github.com>
2019-10-27 15:57:12 -0400

    Created the start of the player class

---
+++ items/player.py
@@ -0,0 +1,23 @@
+"""
+The player class
+
+"""
+import pygame
+
+from items.item import Item
+
+
+class Player(Item):
+    """
+    Subclass of the item class
+    """
+
+    def __init__(self, position: tuple, size: tuple):
+        super(Player, self).__init__(position, (255, 0, 0), size)
+
+    def render(self, scene: pygame.Surface) -> None:
+        """
+        :param scene: Main scene the game is showing
+        """
+        dimensions = (self.x, self.y, self.width, self.height)
+        pygame.draw.rect(scene, self.color, dimensions)
